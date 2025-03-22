063576863fd55052804671bcb3679dfb8813d6dc
Siddhant753 <siddhantchakre7@gmail.com>
2025-03-22 19:57:04 +0530

    Time: 11 ms (72.68%) | Memory: 17.8 MB (86.49%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,13 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        HM1, HM2 = {}, {}
         n, m = len(s), len(t)
+        HM = {}
         if n != m:
             return False
-        for i, j in zip(s, t):
-            HM1[i] = HM1.get(i, 0) + 1
-            HM2[j] = HM2.get(j, 0) + 1
-        if HM1 == HM2:
-            return True
-        else:
-            return False
+        for char in s:
+            HM[char] = HM.get(char, 0) + 1
+        for char in t:
+            if char not in HM or HM[char] == 0:
+                return False
+            HM[char] -= 1
+        return True
