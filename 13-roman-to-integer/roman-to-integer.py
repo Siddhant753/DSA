38ab63838830c47cdef17b3ad6749275f2b9c004
Siddhant753 <siddhantchakre7@gmail.com>
2025-03-23 20:11:16 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (40.46%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -4,10 +4,12 @@ class Solution:
             "I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000
         }
         result = 0
-        n = len(s)
-        for i in range(n):
-            if i < n-1 and roman_value[s[i]] < roman_value[s[i+1]]:
-                result -= roman_value[s[i]]
+        pre_val = 0
+        for char in reversed(s):
+            value = roman_value[char]
+            if value < pre_val:
+                result -= value
             else:
-                result += roman_value[s[i]]
+                result += value
+            pre_val = value
         return result
