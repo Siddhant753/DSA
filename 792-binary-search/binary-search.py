7ddfdb178d7b5897e133ab1c7c992daa9cd8e31f
Siddhant753 <siddhantchakre7@gmail.com>
2025-03-27 20:30:12 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (6.36%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,17 +1,13 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-
+        left = 0
+        right = len(nums) - 1
         while left <= right:
-            mid = (left + right) // 2  # Find the middle index
-        
+            mid = (left + right) // 2
             if nums[mid] == target:
-                return mid  # Target found
-        
+                return mid
             elif nums[mid] < target:
-                left = mid + 1  # Search in the right half
-            
+                left = mid + 1
             else:
-                right = mid - 1  # Search in the left half
-
+                right = mid - 1
         return -1
