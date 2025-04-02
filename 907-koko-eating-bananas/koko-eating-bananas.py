0ebf8164776099a4432760f8efb2960dfd98b677
Siddhant753 <siddhantchakre7@gmail.com>
2025-04-02 20:30:43 +0530

    Time: 156 ms (61.79%) | Memory: 18.7 MB (93.22%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,24 +1,14 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        #high = 0
-        #for i in piles:
-        #    high = max(high, i)
-        
-        high = max(piles) # Directly findes maximum element in array
+        left, right = 1, max(piles)
 
-        def totalHours(piles, hourly):
-            total_hour = 0
-            n = len(piles)
-            for i in range(n):
-                total_hour += math.ceil(piles[i] / hourly)
-            return total_hour
+        def totalHours(k):
+            return sum((bananas + k - 1) // k for bananas in piles)
 
-        left, right = 1, high
-        while left <= right:
+        while left < right:
             mid = (left + right) // 2
-            total_hour = totalHours(piles, mid)
-            if total_hour <= h:
-                right = mid - 1
+            if totalHours(mid) <= h:
+                right = mid
             else:
                 left = mid + 1
         return left
