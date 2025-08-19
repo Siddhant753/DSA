8276ba6cf8be3ab32ac5233d72492ea0c46d5d0e
Siddhant753 <siddhantchakre7@gmail.com>
2025-08-19 18:13:42 +0530

    Time: 43 ms (31.47%) | Memory: 17.9 MB (36.75%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -9,7 +9,7 @@ class Solution:
         if x < 0:
             rev = -rev
 
-        if -2 ** 31 > rev  or rev > 2 ** 31 - 1:
+        if rev < -2**31 or rev > 2**31 - 1:
             return 0
         
         return rev
